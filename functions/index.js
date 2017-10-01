d2739d4c82a0509f8ac1b502c2935caa135534e9
peepitkm <pornpat.pp@gmail.com>
2017-10-01 14:11:26 +0700

    Add order page

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+
+exports.addMenus = functions.https.onRequest((req, res) => {
+
+  const item_name = req.query.name;
+  const item_no = req.query.no;
+  const item_type = req.query.type;
+  const item_category_id = req.query.category_id;
+  const item_price = req.query.price;
+
+  let new_menu = {};
+  let name = 'name';
+  let no = 'no';
+  let type = 'type';
+  let category = 'category';
+  let price = 'price';
+  new_menu[name] = item_name;
+  new_menu[no] = item_no;
+  new_menu[type] = item_type;
+  new_menu[category] = item_category_id;
+  new_menu[price] = item_price;
+
+  admin.database().ref('/menus').push(new_menu).then(snapshot => {
+    console.log('A new menu item was added');
+  });
+});
